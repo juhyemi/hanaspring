e39961e2f7a4c927e69d31bfb3bc6023d1f7129d
juhye <rlawngp124@gmail.com>
2024-03-26 14:19:07 +0900

    20240326

---
+++ sb1/src/main/java/com/hana/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.hana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main(){
+        return "main";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
